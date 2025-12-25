9bbca8c4901be1df4445ad6bdbb5efeea8eb53de
DarioD <dario.demol@outlook.be>
2025-12-25 18:24:14 +0100

    Refactor movement validators import in routes
    Replaces inline movementValidators definition with an import from '../middlewares/validators' for improved code organization and reusability.

--- src/routes/stockMovement.routes.js
+++ src/routes/stockMovement.routes.js
@@ -2,14 +2,7 @@ const express = require('express');
 const router = express.Router();
 const StockMovementController = require('../controllers/stockMovement.controller');
 const { validate } = require('../middlewares/validation.middleware');
-const { body } = require('express-validator');
-
-const movementValidators = [
-    body('product_id').isInt().withMessage('Product ID must be an integer'),
-    body('type').isIn(['IN', 'OUT', 'ADJUSTMENT']).withMessage('Type must be IN, OUT, or ADJUSTMENT'),
-    body('quantity').isInt({ gt: 0 }).withMessage('Quantity must be greater than 0'),
-    body('description').optional().isString()
-];
+const { movementValidators } = require('../middlewares/validators');
 
 router.get('/', StockMovementController.getAll);
 router.post('/', movementValidators, validate, StockMovementController.create);
